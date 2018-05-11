2caaed79b96a972891b3b240671f29c4310ffb14
MSCapeletti <matheus_capeletti@hotmail.com>
2018-05-11 17:41:14 -0300

    Painel do Cardapio versão alpha release 0.0.1

--- Visual/Desktop/src/model/Administrador.java
+++ Visual/Desktop/src/model/Administrador.java
@@ -15,42 +15,11 @@ public class Administrador extends Funcionario {
     }
     
     //deve adicionar um novo produto ao cardapio
-    public Cardapio adicionarAoCardapio(Cardapio cardapio) {
-        s = new Scanner(System.in);
-        String[] ingredientes;
-        ingredientes = new String[numIngredientes];
-        Produto p = new Produto(numIngredientes, 1);
-        
-        System.out.println("Digite 1 para adicionar pizza, 2 para bebida");
-        
-        switch(s.nextInt()) {
-            case 1:
-                
-                for(int i = 0; i < 20; i++) {
-                    ingredientes[i] = s.nextLine();
-                }
-                
-                //p = new Pizza(s.nextLine(), s.nextFloat(), ingredientes, s.nextInt(), s.nextInt(), s.nextLine());
-                cardapio.adicionarAoCardapio(p);
-                
-                break;
-            case 2:
-                
-                //p = new Bebida(s.nextLine(), s.nextFloat(), s.nextInt(), s.nextLine());
-                cardapio.adicionarAoCardapio(p);
-                
-                break;
-        }
-        s = null;
-        return cardapio;
+    public void adicionarAoCardapio(Cardapio cardapio, Produto produto) {
+        cardapio.adicionarAoCardapio(produto);
     }
     
-    public Cardapio removerDoCardapio(Cardapio cardapio) {
-        //if(produto nao existe no cardapio)
-        
-        //else
-        //cardapio.remove(produto);
-        
-        return cardapio;
+    public void removerDoCardapio(Cardapio cardapio, Produto produto) {
+        cardapio.removerDoCardapio(produto);
     }
 }
